58829750db1c30f61938bd030d8e3d1c401225f5
Keith Crain <kwcjr@users.noreply.github.com>
2021-03-19 22:59:38 -0400

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,71 @@
+<footer>
+			<div class="container mt-5">
+				<div class="d-flex pt-4 pb-4">
+					<div class="p-0 flex-fill">
+						<img width="52" height="52" src="https://cdn.shortpixel.ai/spai/w_65+q_lossy+ret_img+to_webp/https://mainwp.com/wp-content/uploads/2017/03/MainWP-Icon-85.png" data-spai="1" class="attachment-thumbnail size-thumbnail" alt="MainWP" sizes="(max-width: 85px) 100vw, 85px" data-spai-upd="52">
+						<div class="slogan"><strong>WordPress Management</strong><br>for Professionals</div>
+					</div>
+					<div class="p-1 flex-fill">
+						<h3 style="font-weight:bold">MainWP</h3>
+						<ul class="footer-menu">
+							<li><a href="https://mainwp.com/mainwp-blog/">MainWP Blog</a></li>
+							<li><a href="https://mainwp.com/signup/">MainWP Pro</a></li>
+							<li><a href="https://mainwp.com/mainwp-extensions/">MainWP Extensions</a></li>
+							<li><a href="https://mainwp.com/protect-your-data/">Data Protection</a></li>
+							<li><a href="https://mainwp.com/mainwp-roadmaps/">MainWP Roadmaps</a></li>
+							<li><a href="https://mainwp.com/about/">About MainWP</a></li>
+						</ul>
+					</div>
+					<div class="p-1 flex-fill">
+						<h3 style="font-weight:bold">Help</h3>
+						<ul class="footer-menu">
+							<li><a href="https://mainwp.com/preinstall-faq/">Pre-install FAQ</a></li>
+							<li><a href="https://mainwp.com/support/">MainWP Support</a></li>
+							<li><a href="https://mainwp.com/contact/">Contact MainWP</a></li>
+							<li><a href="https://mainwp.com/help/docs/system-requirements/">System Requirements</a></li>
+							<li><a href="https://mainwp.com/help/docs/hosting-compatibility/">Hosting Compatibility</a></li>
+							<li><a href="https://mainwp.com/retired-extensions/">Retired MainWP Extensions</a></li>
+							<li><a href="https://mainwp.com/value-added-tax-vat-faqs/">Value Added Tax (VAT) FAQs</a></li>
+						</ul>
+					</div>
+					<div class="p-1 flex-fill">
+						<h3 style="font-weight:bold">We Recommend</h3>
+						<ul class="footer-menu">
+							<li><a href="https://wordpress.org/support/view/plugin-reviews/mainwp?filter=5">MainWP Reviews</a></li>
+							<li><a href="https://mainwp.com/mainwp-exclusives/">Exclusive Discounts</a></li>
+							<li><a href="https://mainwp.com/white-hat-reward/">White Hat Reward</a></li>
+							<li><a href="https://mainwp.com/contributors/">Contributors</a></li>
+							<li><a href="https://mainwp.com/mainwp-coupon/">MainWP Coupon</a></li>
+							<li><a href="https://mainwp.com/mainwp-affiliates/">MainWP Affiliates</a></li>
+							<li><a href="https://mainwp.com/wordcamps/">WordCamps</a></li>
+						</ul>
+					</div>
+					<div class="p-1 flex-fill">
+						<h3 style="font-weight:bold">Developers</h3>
+						<ul class="footer-menu">
+							<li><a href="https://github.com/mainwp/mainwp/blob/master/.github/CONTRIBUTING.md">Contributing to MainWP</a></li>
+							<li><a href="https://mainwp.dev">MainWP Developers</a></li>
+							<li><a href="https://mainwp.com/github/">MainWP GitHub</a></li>
+							<li><a href="https://mainwp.com/creating-a-wordpress-plugin/">Create an Extension</a></li>
+							<li><a href="https://github.com/mainwp/mainwp-hello-world-extension/">Hello World Extension</a></li>
+							<li><a href="https://mainwp.com/mainwp-experts/">MainWP Experts</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			<div class="p-3 footer-meta text-white d-flex justify-content-between">
+				<span class="copyright">© MainWP - WordPress Management for Professionals 2014 - 2020 -
+					<a href="https://mainwp.com/terms-of-service/">Terms of Service</a> -
+					<a href="https://mainwp.com/privacy-policy/">Privacy Policy</a> -
+					<a href="https://mainwp.com/cookie-policy/">Cookie Policy</a> -
+					<a href="https://mainwp.com/support-policy/">Support Policy</a> -
+					<a href="https://mainwp.com/refund-policy/">Refund Policy</a> -
+					<a href="https://mainwp.com/mainwp-domain-use/">MainWP Domain Use</a>
+				</span>
+				<span class="footer-meta-social">
+					<a href="https://www.facebook.com/mainwp"><i class="fab fa-facebook"></i></a>
+					<a href="https://twitter.com/mymainwp"><i class="fab fa-twitter"></i></a>
+					<a href="https://meta.mainwp.com/"><i class="fab fa-discourse"></i></a>
+				</span>
+			</div>
+		</footer>
